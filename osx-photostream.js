38b8519769a0135a3f9a36f065f6ea1e64f8bd47
Franklin van de Meent <fr@nkl.in>
2014-03-10 23:42:22 +0100

    And there was Code

---
+++ osx-photostream.js
@@ -0,0 +1,67 @@
+/*
+Name:          osx-photostream
+Description:   Node.js module to realtime process iCloud PhotoStream photos on OSX
+Author:        Franklin van de Meent (https://frankl.in)
+Source:        https://github.com/fvdm/nodejs-osx-photostream
+Feedback:      https://github.com/fvdm/nodejs-osx-photostream/issues
+License:       Unlicense / Public domain (see LICENSE file)
+*/
+
+var fs = require('fs') 
+var EE = require('events').EventEmitter
+var exec = require('child_process').exec
+
+var app = new EE()
+
+app.watchPath = process.env.HOME +'/Library/Application Support/iLifeAssetManagement/assets/sub'
+
+module.exports = function( dest ) {
+	app.writeDest = dest ? dest.replace( /^~(\/.*)/, process.env.HOME +'$1' ) : false
+	doWatch()
+	return app
+}
+
+function doWatch() {
+	fs.watch( app.watchPath, function( event, dirname ) {
+		if( event === 'rename' && dirname.match(/^[a-z0-9]{42}$/) ) {
+			fs.readdir( app.watchPath +'/'+ dirname, function( err, files ) {
+				if( !err ) {
+					for( var f in files ) {
+						var file = {}
+						file.pathname = app.watchPath +'/'+ dirname
+						file.filename = files[f]
+						file.fullpath = file.pathname +'/'+ file.filename
+						
+						app.emit( 'update', file )
+						
+						if( app.writeDest ) {
+							file.copypath = app.writeDest +'/'+ files[f]
+							
+							exec(
+								'/bin/cp -p '+ cmdescape(file.fullpath) +' '+ cmdescape(file.copypath),
+								{ timeout: 5000 },
+								function( err, stdout, stderr ) {
+									if( err ) {
+										app.emit( 'fail', 'copy failed', err, file )
+									} else {
+										app.emit( 'copy', file )
+									}
+								}
+							)
+						}
+					}
+				} else {
+					app.emit( 'fail', 'readdir failed', err, app.watchPath +'/'+ dirname )
+				}
+			})
+			
+			delete files
+		} else {
+			app.emit( 'misc', event, dirname )
+		}
+	})
+}
+
+function cmdescape( str ) {
+	return str.replace( /([\\$'"`\s])/g, '\\$1' )
+}
